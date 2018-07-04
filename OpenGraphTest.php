bb151aaaadeeec749b00d0d31b6803d0f6c4c36d
Zuruh <nzureuz@gmail.com>
2018-07-04 13:54:37 -0600

    Simple Data Display
    Simplify the data display on php. 
    Removed \\u0000 when converting to Array object

--- OpenGraphTest.php
+++ OpenGraphTest.php
@@ -1,31 +1,25 @@
 <?php
-require 'OpenGraph.php';
-
-class OpenGraphTest extends PHPUnit_Framework_TestCase
-{
-    public function testFetch()
-    {
-        $o = OpenGraph::fetch(
-          'http://www.rottentomatoes.com/m/10011268-oceans/'
-        );
-
-        $this->assertType('OpenGraph', $o);
-
-        $this->assertAttributeEquals(
-          array(
-            'title'     => 'Oceans',
-            'type'      => 'movie',
-            'image'     => 'http://images.rottentomatoes.com/images/movie/custom/68/10011268.jpg',
-            'url'       => 'http://www.rottentomatoes.com/m/10011268-oceans/',
-            'site_name' => 'Rotten Tomatoes',
-          ),
-          '_values',
-          $o
-        );
-    }
-
-    public function testFetchReturnsFalseForWebsiteWithNoOpenGraphMetadata()
-    {
-        $this->assertEquals(FALSE, OpenGraph::fetch('http://www.example.org/'));
-    }
-}
+  require 'OpenGraph.php';
+  $o = OpenGraph::fetch(
+    'http://hashtiv.link/7n55ZnyVim'
+  );
+  
+  $array =  (array) $o;
+  $array = str_replace('\\u0000', "", json_encode($array));
+  $array = json_decode($array, true);
+  print_r($array);
+  echo('<br><br>');
+  echo('title:'.$array['OpenGraph_values']['title'].'<br>');
+  echo('description:'.$array['OpenGraph_values']['description'].'<br>');
+  echo('image:'.$array['OpenGraph_values']['image'].'<br>');
+  if (array_key_exists("twitter_card",$array['OpenGraph_values'])){
+    echo ('twitter_card:'.$array['OpenGraph_values']['twitter_card'].'<br>');
+  }
+  echo('<br><br>');
+  echo('hashtiv_title:'.$array['OpenGraph_values']['hashtiv_title'].'<br>');
+  echo('hashtiv_description:'.$array['OpenGraph_values']['hashtiv_description'].'<br>');
+  echo('hashtiv_image:'.$array['OpenGraph_values']['hashtiv_image'].'<br>');
+  if (array_key_exists("hashtiv_display",$array['OpenGraph_values'])){
+    echo ('hashtiv_display:'.$array['OpenGraph_values']['hashtiv_display'].'<br>');
+  }
+?>
